5aea6dda1906adef40ddfef0f23ed095c9259081
firasbelhiba <firas.belhiba@esprit.tn>
2021-06-27 14:17:10 +0100

    Adding Header

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Header = (props) => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>{props.title}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 90,
+        paddingTop: 36,
+        backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerTitle: {
+        color: 'white',
+        fontSize: 18,
+
+    }
+});
+
+export default Header
