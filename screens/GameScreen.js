64675d066cc85a5602e3e4d9b01640159fed176b
firasbelhiba <firas.belhiba@esprit.tn>
2021-06-27 18:02:29 +0100

    adding logic to our came component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,6 @@
 
-import React, { useState } from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useRef } from 'react'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import Card from '../components/Card';
 import NumberContainer from '../components/NumberContainer';
 
@@ -19,6 +19,24 @@ const generateRandomBetween = (min, max, exclude) => {
 
 const GameScreen = (props) => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice))
+
+    const currentLow = useRef(1)
+    const currentHigh = useRef(100)
+
+    const nextgessHandler = direction => {
+        if ((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'greater' && currentGuess > props.userChoice)) {
+            Alert.alert('Don t lie ! ', 'Comon you know that you are cheating ... ', [{ text: 'Sorry ! ', style: 'cancel' }])
+            return
+        }
+        if (direction === 'lower') {
+            currentHigh.current = currentGuess
+        } else {
+            currentLow.current = currentGuess
+
+        }
+        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
+        setCurrentGuess(nextNumber)
+    }
     return (
         <View style={styles.screen}>
             <Text>Openent's Guess</Text>
@@ -26,8 +44,8 @@ const GameScreen = (props) => {
                 {currentGuess}
             </NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title='Lower' />
-                <Button title='Greater' />
+                <Button title='Lower' onPress={nextgessHandler.bind(this, 'lower')} />
+                <Button title='Greater' onPress={nextgessHandler.bind(this, 'greater')} />
             </Card>
         </View>
     )
